7fc24c57dc6a49ed16167415b61fa4bd13451a44
Israel <israel.lewis@mnscorp.net>
2017-06-13 09:47:47 +0100

    teams and api query successful

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var get = require('simple-get');
 
 // Setup Restify Server
 var server = restify.createServer();
+// server.use(restify.queryParser());
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
@@ -19,15 +21,14 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    // session.send("You said: %s", teamQ(session.message.text));
-    var daResponse = teamQ(session.message.text);
-    session.send(daResponse);
+	const opts = {
+  			method: 'GET',
+  			url: 'http://askpercy.herokuapp.com/v1/percyp/'+ session.message.text.toLowerCase(),
+  			json: true
+	}
+	get.concat(opts, function (err, res, data) {
+  		if (err) throw err
+  		console.log(data.explanation) // `data` is an object
+  		session.send(data.explanation);
+	})
 });
-
-function teamQ(searchTerm) {
-	return $http.get("https://askpercy.herokuapp.com/v1/percyp/" + searchTerm.toLowerCase()).then(function (resp) {
-        return resp.data;
-    }).catch(function() {
-        return $q.reject(_error);
-    });
-};
